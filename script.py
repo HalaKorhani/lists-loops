22f038d37d36daeb764bb5bf14f66b0027bd6fca
HalaKorhani <halakorhani2005@icloud.com>
2025-02-19 07:42:18 +0200

    getting info

--- script.py
+++ script.py
@@ -1,2 +1,12 @@
 import random 
 list=[]
+salary=int(input("input your salary for the month"))
+list.append(salary)
+month=input("enter the name of the month")
+list.append(month)
+psavings=float(input("Enter the percentage of salary for savings:"))
+list.append(psavings)
+pRent=float(input("Enter the percentage of salary for Rent:"))
+list.append(pRent)
+pelectricity= float(input("Enter the percentage of salary for electricity:"))
+list.append(pelectricity)
